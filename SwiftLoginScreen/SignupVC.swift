71af0c3efcefc500332ec415bcd0761d3bba143b
jafriyie1 <sfjafthemp@gmail.com>
2016-07-17 12:06:33 -0500

    Initial Changes

---
+++ SwiftLoginScreen/SignupVC.swift
@@ -0,0 +1,45 @@
+//
+//  SignupVC.swift
+//  SwiftLoginScreen
+//
+//  Created by Dipin Krishna on 31/07/14.
+//  Copyright (c) 2014 Dipin Krishna. All rights reserved.
+//
+
+import UIKit
+
+class SignupVC: UIViewController {
+    
+    @IBOutlet var txtUsername : UITextField!
+    @IBOutlet var txtPassword : UITextField!
+    @IBOutlet var txtConfirmPassword : UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+    // #pragma mark - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func gotoLogin(sender : UIButton) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    @IBAction func signupTapped(sender : UIButton) {
+        }
+}
